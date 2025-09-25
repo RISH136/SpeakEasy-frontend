e2d6b134715ead06057ceb15c19f22eeff939e55
RISH136 <harsh03092003@gmail.com>
2025-09-25 13:05:54 +0530

    third commit

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://speakeasy-backend-1.onrender.com";
+const BASE_URL = "https://speakeasy-backend-1.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
